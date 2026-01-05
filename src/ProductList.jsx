7934a8ef22e9216c64dd20bd2a70813db6fb75c3
Edrey Pomarez <pomarezedrey@gmail.com>
2026-01-04 21:51:02 -0600

    Featu: Add add to cart functionality

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,10 +1,22 @@
 import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import './ProductList.css'
 import CartItem from './CartItem';
+import addItem from './CartSlice';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
+    const dispatch = useDispatch();
+
+    const handleAddToCart = function (product) {
+        dispatch(addItem(product));
+
+        setAddedToCart((prevState) => ({
+            ...prevState,
+            [product.name]: true,
+        }));
+    };
 
     const plantsArray = [
         {
